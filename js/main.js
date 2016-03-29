270adadb0ebf00b73dc72abfd11b8b93fc6dff2f
goug0035 <marygough@Marys-MacBook-Pro.local>
2016-03-29 16:53:14 -0400

    basic

---
+++ js/main.js
@@ -0,0 +1,37 @@
+var $body = $('body');
+
+var $btnShowHide = $('.btn-show-hide');
+var $box = $('.box');
+
+var $btnMove = $('.btn-move');
+var $diamond = $('.diamond');
+
+var $btnColEx = $('.btn-collapse-expand');
+var $panel = $('.panel');
+
+var $btnBounce = $('.btn-bounce');
+var $circle = $('.circle');
+
+var $btnAppend = $('.btn-append');
+var $list = $('.list');
+
+
+$btnShowHide.on('click', function () {
+  $box.toggleClass('showbox');
+});
+
+$btnMove.on('click', function () {
+  $diamond.toggleClass('movein');
+});
+
+$btnColEx.on('click', function () {
+  $panel.toggleClass('collapse');
+});
+
+$btnBounce.on('click', function () {
+  $circle.toggleClass('bounce');
+});
+
+$btnAppend.on('click', function () {
+  $list.toggleClass('add');
+});
